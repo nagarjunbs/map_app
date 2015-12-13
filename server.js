7053c2d9bb09d9b6e3f8426ce131b8e8b900295a
nagarjunbs <nagarjun323@gmail.com>
2015-12-13 23:05:29 +0530

    comment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Dependencies
 // -----------------------------------------------------
 var express         = require('express');
-var mongoose        = require('mongoose');
+//var mongoose        = require('mongoose');
 var port            = process.env.PORT || 3000;
 var morgan          = require('morgan');
 var bodyParser      = require('body-parser');
